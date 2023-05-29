1040b8d0d9a9d44333973bf493619050342f106c
Diwas10 <rimaldiwas10@gmail.com>
2023-05-29 21:18:19 +0545

    Size the flash card based on its content's height
    * Set the min card height to 200px
    * Change the card's height when:
        - window resizes
        - card content changes

--- src/components/FlashCard.jsx
+++ src/components/FlashCard.jsx
@@ -1,15 +1,40 @@
-import { useState } from "react";
+import { useState, useEffect, useRef } from "react";
 import "../styles/FlashCard.css";
+
+const MIN_CARD_HEIGHT = 200;
 
 export default function FlashCard({ question, options, answer }) {
   const [flipped, setFlipped] = useState(false);
+  const [cardHeight, setCardHeight] = useState(MIN_CARD_HEIGHT);
+  const frontSide = useRef(null);
+  const backSide = useRef(null);
+
+  // Resize the card when window resizes
+  useEffect(() => {
+    window.addEventListener("resize", resizeCard);
+    return () => window.removeEventListener("resize", resizeCard);
+  }, []);
+
+  // Resize the card when props change
+  useEffect(() => {
+    resizeCard();
+  }, [question, options, answer]);
+
+  // Set the card's height to max of front height, back height and 200px
+  function resizeCard() {
+    const frontHeight = frontSide.current.getBoundingClientRect().height;
+    const backHeight = backSide.current.getBoundingClientRect().height;
+    const maxHeight = Math.max(frontHeight, backHeight, MIN_CARD_HEIGHT);
+    setCardHeight(maxHeight);
+  }
 
   return (
     <div
       className={`flash-card ${flipped ? "flipped" : ""}`}
+      style={{ height: `${cardHeight}px` }}
       onClick={() => setFlipped(!flipped)}
     >
-      <div className="front">
+      <div className="front" ref={frontSide}>
         {question}
         <ul>
           {options.map((option, i) => (
@@ -17,7 +42,9 @@ export default function FlashCard({ question, options, answer }) {
           ))}
         </ul>
       </div>
-      <div className="back">{answer}</div>
+      <div className="back" ref={backSide}>
+        {answer}
+      </div>
     </div>
   );
 }
